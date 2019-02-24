835104a822bb144a11038233d026837fec8d4c0e
maximars <maximars@ukr.net>
2019-02-24 22:59:54 +0200

    initial commit

---
+++ GoJavaOnlineFirstProgram.java
@@ -0,0 +1,5 @@
+public class GoJavaOnlineFirstProgram {
+	public static void main(String[] args){
+		System.out.println("Welcome to java online course:");
+			}
+}
---
+++ src/GoJavaOnline.java
@@ -0,0 +1,5 @@
+public class GoJavaOnline  {
+        public static void main(String[] args){
+        System.out.println("Welcome to java online course from within Intelleji IDEA:");
+    }
+}
